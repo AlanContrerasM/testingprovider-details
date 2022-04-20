985e2a3ac6625cad3787cd2f45669f0fbf84c92c
Ilya Sorokin <dev1@iLiveMyLife.io>
2022-04-20 14:25:55 -0700

    Finish widget examples

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,193 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
+import 'dart:async';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    //4. wrap with notifierProvider
+    ChangeNotifierProvider(
+      create: (_) => ObjectProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(),
+      ),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Home Page'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      body: Column(children: [
+        Row(
+          children: const [
+            Expanded(child: CheapWidget()),
+            Expanded(child: ExpensiveWidget()),
+          ],
+        ),
+        Row(children: const [Expanded(child: ObjectProviderWidget())]),
+        Row(
+          children: [
+            TextButton(
+              onPressed: () {
+                context.read<ObjectProvider>().start();
+              },
+              child: const Text('Start'),
+            ),
+            TextButton(
+              onPressed: () {
+                context.read<ObjectProvider>().stop();
+              },
+              child: const Text('Stop'),
+            ),
+          ],
+        )
+      ]),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+//1. create a base class, the expensive and cheap widget will extend it.
+@immutable
+class BaseObject {
+  final String id;
+  final String lastUpdated;
+  BaseObject()
+      : id = const Uuid().v4(),
+        lastUpdated = DateTime.now().toIso8601String();
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  bool operator ==(covariant BaseObject other) => id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+//Expensive Widget
+@immutable
+class ExpensiveObject extends BaseObject {}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+//Cheap Widget
+@immutable
+class CheapoObject extends BaseObject {}
+
+class ObjectProvider extends ChangeNotifier {
+  late String id;
+  late CheapoObject _cheapObject;
+  late StreamSubscription _cheapObjectStreamSubs;
+  late ExpensiveObject _expensiveObject;
+  late StreamSubscription _expensiveObjectStreamSubs;
+
+  CheapoObject get cheapObject => _cheapObject;
+  ExpensiveObject get expensiveObject => _expensiveObject;
+
+  //provider constructor
+  ObjectProvider()
+      : id = const Uuid().v4(),
+        _cheapObject = CheapoObject(),
+        _expensiveObject = ExpensiveObject() {
+    start();
+  }
+
+  //lets override our notifyListener so we reset id everytime
+  @override
+  void notifyListeners() {
+    id = const Uuid().v4();
+    super.notifyListeners();
+  }
+
+  void start() {
+    _cheapObjectStreamSubs = Stream.periodic(
+      const Duration(seconds: 1),
+    ).listen((event) {
+      //every second we are replacing the object with a new one.
+      _cheapObject = CheapoObject();
+      notifyListeners();
+    });
+
+    _expensiveObjectStreamSubs = Stream.periodic(
+      const Duration(seconds: 10),
+    ).listen((event) {
+      //every 10 seconds we are replacing the object with a new one.
+      _expensiveObject = ExpensiveObject();
+      notifyListeners();
     });
   }
 
+  void stop() {
+    _cheapObjectStreamSubs.cancel();
+    _expensiveObjectStreamSubs.cancel();
+  }
+}
+
+class ExpensiveWidget extends StatelessWidget {
+  const ExpensiveWidget({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    final expensiveObject = context.select<ObjectProvider, ExpensiveObject>(
+      (provider) => provider.expensiveObject,
+    );
+    return Container(
+      height: 100,
+      color: Colors.blue,
+      child: Column(children: [
+        const Text("Expensive Widget"),
+        const Text("Last Updated"),
+        Text(expensiveObject.lastUpdated)
+      ]),
+    );
+  }
+}
+
+class CheapWidget extends StatelessWidget {
+  const CheapWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //you choose what you want as second paramenter.
+    final cheapObject = context.select<ObjectProvider, CheapoObject>(
+      (provider) => provider.cheapObject,
+    );
+    return Container(
+      height: 100,
+      color: Colors.orange,
+      child: Column(children: [
+        const Text("Cheap Widget"),
+        const Text("Last Updated"),
+        Text(cheapObject.lastUpdated)
+      ]),
+    );
+  }
+}
+
+class ObjectProviderWidget extends StatelessWidget {
+  const ObjectProviderWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = context.watch<ObjectProvider>();
+
+    return Container(
+      height: 100,
+      color: Colors.purple,
+      child: Column(children: [
+        const Text("Object Provider Widget"),
+        const Text("Id"),
+        Text(provider.id)
+      ]),
     );
   }
 }
